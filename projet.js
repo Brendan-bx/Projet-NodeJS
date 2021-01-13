4af10d4da238adb2fedb0df3fa3cf2ab3195af9a
Brendan <brendanbx@gmail.com>
2021-01-13 16:43:19 +0100

    quasi story 2

--- projet.js
+++ projet.js
@@ -1,3 +1,5 @@
+const { title } = require('process');
+
 console.log(process.argv)
 if (process.argv[3] == "exit") {
     process.on('exit', function(code) {
@@ -5,10 +7,24 @@ if (process.argv[3] == "exit") {
     });
 }
 
+
+
+while ([19554]) {
 if (process.argv[3] == "transform") {
     const fs = require('fs')
-    fs.readFile('movies.json', function(erreur, fichier) {
-   let movies = JSON.parse(fichier)
-   console.log(movies[1])
+    fs.readFile('movies.json', function transform (erreur, fichier) {
+    let movies = JSON.parse(fichier)
+    //console.log(movies[2].release_date)
+    
+    let date = new Date (movies[1].release_date*1000);
+
+
+    let donnees = JSON.stringify(`movies:${movies[1].title} (${date.getFullYear()}) release date : ${movies[1].release_date}`)
+ 
+    fs.writeFile('movies.out.json', donnees, function(erreur) {
+    if (erreur) {
+        console.log(erreur)}
+
 })
-}
+})
+}}
