ba3cf06ad62b5a71103e7986fb5cb0eb70512cf9
Brendan <brendanbx@gmail.com>
2021-01-13 14:08:23 +0100

    story 1 et début story 2

--- projet.js
+++ projet.js
@@ -1,1 +1,14 @@
-uogyuhjl
+console.log(process.argv)
+if (process.argv[3] == "exit") {
+    process.on('exit', function(code) {
+        return console.log(`About to exit with code ${code}`);
+    });
+}
+
+if (process.argv[3] == "transform") {
+    const fs = require('fs')
+    fs.readFile('movies.json', function(erreur, fichier) {
+   let movies = JSON.parse(fichier)
+   console.log(movies[1])
+})
+}
