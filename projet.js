6541e8e8add8cff8680b668ce6fbab0b3fe2b853
Brendan <brendanbx@gmail.com>
2021-01-14 16:11:53 +0100

    début story 4

--- projet.js
+++ projet.js
@@ -11,21 +11,46 @@ if (process.argv[3] == "exit") {
 const fs = require('fs');
 const { domainToUnicode } = require('url');
 //let test= 0;
-
+let date;
+let Year;
+let array = []
+let movies;
+let fichierEcriture;
 
 switch (process.argv[3]) {
     case ("transform"):
-        let array = []
-        let movies = require(process.argv[4])
-        let fichierEcriture = process.argv[5];
+        movies = require(process.argv[4])
+        fichierEcriture = process.argv[5];
         for (i = 0; i < movies.length; i++) {
             //console.log(movies[2].release_date)
-
-            let date = new Date(movies[i].release_date * 1000);
-            let Year = date.getFullYear();
+            date = new Date(movies[i].release_date * 1000);
+            Year = date.getFullYear();
             movies[i].title = movies[i].title + " " + "(" + Year + ")";
             writable(movies[i], fichierEcriture);
         }
+        break;
+
+
+    case ("sort_date"):
+        movies = require(process.argv[4])
+        fichierEcriture = process.argv[5];
+        for (i = t.length - 1; i >= 1; i--) {
+            for (j = 0; j <= i - 1; j++) {
+                date = new Date(movies[i].release_date * 1000);
+                Year = date.getFullYear();
+                if (t[j + 1] < t[j]) {
+                    let tmp = t[j];
+                    t[j] = t[j + 1]
+                    t[j + 1] = tmp
+
+                }
+                movies[i].title = movies[i].title + " " + "(" + Year + ")";
+                writable(movies[i], fichierEcriture);
+            }
+
+
+        }
+
         break;
 
 
@@ -40,4 +65,8 @@ function writable(fichierLEcture, fichierEcriture) {
 };
 
 let stop = new Date().getTime();
-console.log("time : " + (stop - start));
+console.log("temps d'éxécution : " + (stop - start) + " ms ");
+
+
+
+
